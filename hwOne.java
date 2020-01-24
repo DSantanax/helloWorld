8ca2a15e44ac4e485287fda3106b432e3eeee967
Daniel Santana <santana.danny4g@gmail.com>
2020-01-24 09:41:39 -0800

    Updated and fixed misspelled words

--- hwOne.java
+++ hwOne.java
@@ -1,5 +1,5 @@
 public class hwOne {
 	public static void main (String[] args){
-		System.out.println("Hello, World my name is Daniel Santana.");
+		System.out.println("Hello, my name is Daniel Santana.");
 	}
 }
